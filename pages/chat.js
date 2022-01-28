16a4ac56d8ca802bd94ef0374e65d63477cbcba8
Guilherme Sousa <guilhesou@hotmail.com>
2022-01-28 11:27:33 -0400

    Alterações e adições realizadas na Aula 3 + desafios

--- pages/chat.js
+++ pages/chat.js
@@ -1,5 +1,196 @@
+import { Box, Button, Input, Text, Image, IconButton } from "@chakra-ui/react";
+import { MdLogout, MdSend, MdOutlineDelete } from 'react-icons/md'
+import { useState } from "react";
+import appConfig from '../config.json'
+
 export default function Chat() {
+
+
+    /*
+        --------------| Desafio |-------------
+
+        [X] Colocar um botão de enviar
+        [ ] Colocar um botão de deletar mensagem(Usar método filter)
+    
+    */
+
+    const [mensagem, setMensagem] = useState('');
+    const [messages, setmessages] = useState([])
+
+    function handleNewMessage(newMessage) {
+        const message = {
+            id: messages.length + 1,
+            from: 'Guilhermesou',
+            text: newMessage,
+        }
+        setmessages([
+            message,
+            ...messages
+        ]);
+        setMensagem('');
+    }
+
     return (
-        <div><h1>Página do Chat</h1></div>
+        <Box
+            display={'flex'}
+            alignItems={'center'}
+            justifyContent={'center'}
+            backgroundColor={'#1A1A1A'}
+            backgroundImage={`url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)`}
+            backgroundRepeat={'no-repeat'}
+            backgroundSize={'cover'}
+            backgroundBlendMode={'multiply'}
+
+        >
+            <Box
+                display={'flex'}
+                flexDirection={'column'}
+                flex={1}
+                boxShadow={'0 2px 10px 0 rgb(0 0 0 / 20%)'}
+                borderRadius={'5px'}
+                backgroundColor={appConfig.theme.colors.neutrals[700]}
+                height={'100%'}
+                maxW={'95%'}
+                maxH={'95vh'}
+                padding={'32px'}
+
+            >
+                <Header />
+                <Box
+                    position={'relative'}
+                    display={'flex'}
+                    flex={1}
+                    justifyContent={'flex-end'}
+                    height={'80%'}
+                    backgroundColor={appConfig.theme.colors.neutrals[700]}
+                    flexDirection={'column'}
+                    borderRadius={'5px'}
+                    padding={'16px'}
+                >
+
+                    <MessageList messagesData={messages} />
+
+                    <Box
+                        as="form"
+                        display={'flex'}
+                        alignItems={'center'}
+                    >
+                        <Input
+                            value={mensagem}
+                            onKeyPress={(event) => {
+                                if (event.key === 'Enter') {
+                                    event.preventDefault();
+                                    handleNewMessage(mensagem)
+                                }
+                            }}
+                            onChange={(event) => {
+                                const valor = event.target.value
+                                setMensagem(valor)
+                            }}
+                            placeholder="Insira sua mensagem aqui..."
+                            type={'textarea'}
+                            width={'100%'}
+                            border={'0'}
+                            resize={'none'}
+                            borderRadius={'5px'}
+                            padding={'6px 8px'}
+                            backgroundColor={appConfig.theme.colors.neutrals[800]}
+                            mr={'12px'}
+                            textColor={appConfig.theme.colors.neutrals[200]}
+                        />
+                        <IconButton
+                            icon={<MdSend/>}
+                            onClick={() => {handleNewMessage(mensagem)}}
+                        />
+                    </Box>
+
+                </Box>
+
+            </Box>
+        </Box>
     );
+}
+
+
+function Header() {
+    return (
+        <>
+            <Box
+                width={'100%'}
+                marginBottom={'16px'}
+                display={'flex'}
+                alignItems={'center'}
+                justifyContent={'space-between'}
+
+            >
+                <Text textColor={appConfig.theme.colors.neutrals['000']}>
+                    Chat
+                </Text>
+                <Button leftIcon={<MdLogout />} href="/">
+                    Logout
+                </Button>
+
+            </Box>
+        </>
+    )
+}
+
+function MessageList(props) {
+    console.log('MessageList', props.messagesData);
+    return (
+        <Box tag="ul"
+            overflow={'auto'}
+            display={'flex'}
+            flex={1}
+            flexDirection={'column-reverse'}
+            textColor={appConfig.theme.colors.neutrals["000"]}
+            marginBottom={'16px'}
+
+        >
+            {props.messagesData.map((message) => {
+                return (
+                    <Text
+                        key={message.id}
+                        borderRadius={'5px'}
+                        tag={'li'}
+                        padding={'6px'}
+                        marginBottom={'12px'}
+                        
+                    >
+                        <Box
+                            marginBottom={'8px'}
+                            display={'flex'}
+                            flex={1}
+                            flexDirection={'row'}
+                            alignItems={'flex-end'}
+                        >
+                            <Image
+                                width={'30px'}
+                                height={'30px'}
+                                borderRadius={'50%'}
+                                display={'inline-block'}
+                                marginRight={'9px'}
+                                src={'https://github.com/Guilhermesou.png'}
+                            />
+                            <Text tag={'strong'}>
+                                {message.from}
+                            </Text>
+                            <Text
+                                fontSize={'10px'}
+                                marginLeft={'8px'}
+                                textColor={appConfig.theme.colors.neutrals[300]}
+                                tag={"span"}
+                            >
+                                {(new Date().toLocaleDateString())}
+                            </Text>
+                            <IconButton left={'67%'} colorScheme={'white'} icon={<MdOutlineDelete/>}/>
+                        </Box>
+                        {message.text}
+                        
+                    </Text>
+                )
+            })}
+
+        </Box>
+    )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,45 @@
 import appConfig from '../config.json'
-import { ChakraProvider, CSSReset, extendTheme, FormControl } from '@chakra-ui/react'
-import {Box, Button, Text, Input, Image, Form} from '@chakra-ui/react'
+import { Alert, AlertDescription, AlertIcon, AlertTitle, CloseButton } from '@chakra-ui/react'
+import { Box, Button, Text, Input, Image } from '@chakra-ui/react'
 import { useState } from 'react'
 import { useRouter } from 'next/router'
 
-const  theme = extendTheme(appConfig.theme.colors)
-
-
-
 function Titulo(props) {
-    const Tag = props.tag || 'h1';
-    return (
-      <>
-        <Tag>{props.children}</Tag>
-        <style jsx>{`
+  const Tag = props.tag || 'h1';
+  return (
+    <>
+      <Tag>{props.children}</Tag>
+      <style jsx>{`
               ${Tag} {
                   color: ${appConfig.theme.colors.neutrals['000']};
                   font-size: 24px;
                   font-weight: 600;
               }
               `}</style>
-      </>
-    );
-  }
-  
-  // Componente React
-  // function HomePage() {
-  //     // JSX
-  //     return (
-  //         <div>
-  //             <GlobalStyle />
-  //             <Titulo tag="h2">Boas vindas de volta!</Titulo>
-  //             <h2>Discord - Alura Matrix</h2>
-  //         </div>
-  //     )
-  // }
-  // export default HomePage
-  
+    </>
+  );
+}
+
+// Componente React
+// function HomePage() {
+//     // JSX
+//     return (
+//         <div>
+//             <GlobalStyle />
+//             <Titulo tag="h2">Boas vindas de volta!</Titulo>
+//             <h2>Discord - Alura Matrix</h2>
+//         </div>
+//     )
+// }
+// export default HomePage
+
 export default function PaginaInicial() {
-    const [username, setUsername] = useState('Guilhermesou');
-  
-    const router = useRouter()
-    return (
-      <>
-        <Box 
+  const [username, setUsername] = useState('Guilhermesou');
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true)
+  const router = useRouter()
+  return (
+    <>
+      <Box
         display={'flex'}
         alignItems={'center'}
         justifyContent={'center'}
@@ -51,80 +47,83 @@ export default function PaginaInicial() {
 
         backgroundRepeat={'no-repeat'}
         backgroundSize={'cover'}
-        backgroundBlendMode= {'multiply'}
-        >
-          <Box
-            display= {'flex'}
-            alignItems= {'center'}
-            justifyContent= {'space-between'}
-            width={'100%'} 
-            maxWidth={'600px'}
-            borderRadius={'10px'} 
-            padding={'32px'}
-            margin={'16px'}
-            boxShadow={'0 2px 10px 0 rgb(0 0 0 / 20%)'}
-            backgroundColor={appConfig.theme.colors.neutrals[700]}
+        backgroundBlendMode={'multiply'}
+      >
+        <Box
+          display={'flex'}
+          alignItems={'center'}
+          justifyContent={'space-between'}
+          width={'100%'}
+          maxWidth={'600px'}
+          borderRadius={'10px'}
+          padding={'32px'}
+          margin={'16px'}
+          boxShadow={'0 2px 10px 0 rgb(0 0 0 / 20%)'}
+          backgroundColor={appConfig.theme.colors.neutrals[700]}
 
+        >
+          {/* Formulário */}
+          <Box
+            as="form"
+            onSubmit={(event) => {
+              event.preventDefault()
+              router.push('/chat');
+            }}
+            display={'flex'}
+            flexDirection={'column'}
+            alignItems={'center'}
+            justifyContent={'center'}
+            width={{ xs: '100%', sm: '50%' }}
+            textAlign={'center'}
+            marginBottom={'32px'}
           >
-            {/* Formulário */}
-            <Box
-              as="form"
-              onSubmit={(event) => {
-                event.preventDefault()
-                router.push('/chat');
-              }}
-              display={'flex'} 
-              flexDirection={'column'}
-              alignItems={'center'} 
-              justifyContent={'center'}
-              width={ {xs: '100%', sm: '50%'}}
-              textAlign={'center'}
+            <Titulo tag="h2">Boas vindas de volta!</Titulo>
+            <Text
+              variant="body3"
               marginBottom={'32px'}
-            >
-              <Titulo tag="h2">Boas vindas de volta!</Titulo>
-              <Text 
-                variant="body3" 
-                marginBottom={'32px'} 
-                color={appConfig.theme.colors.neutrals[300]}>
-                {appConfig.name}
-              </Text>
-  
-              <Input
-                isFullWidth
-                textColor={appConfig.theme.colors.neutrals[200]}
-                backgroundColor={appConfig.theme.colors.neutrals[800]}
-                borderColor={appConfig.theme.colors.neutrals[800]}
-                value={username}
-                onChange={function (event) {
-                  const valor = event.target.value;
-                  setUsername(valor);
-                }}
-                textStyle={{
-                    neutral: {
-                      mainColor: appConfig.theme.colors.neutrals[900],
-                      mainColorHighlight: appConfig.theme.colors.primary[500],
-                  
-                    }}
+              color={appConfig.theme.colors.neutrals[300]}>
+              {appConfig.name}
+            </Text>
+
+            <Input
+              isFullWidth
+              textColor={appConfig.theme.colors.neutrals[200]}
+              backgroundColor={appConfig.theme.colors.neutrals[800]}
+              borderColor={appConfig.theme.colors.neutrals[800]}
+              value={username}
+              onChange={function (event) {
+                const valor = event.target.value;
+                if (event.target.value.length <= 2) {
+                  setIsButtonDisabled(true)
                 }
-              />
-              <Button
-                marginTop={'25px'}              
-                type='submit'
-                isFullWidth
-                buttonColors={{
-                  contrastColor: appConfig.theme.colors.neutrals["000"],
-                  mainColor: appConfig.theme.colors.primary[500],
-                  mainColorLight: appConfig.theme.colors.primary[400],
-                  mainColorStrong: appConfig.theme.colors.primary[600],
-                }}
-              >Entrar
-              </Button>
-            </Box>
-            {/* Formulário */}
-  
-  
-            {/* Photo Area */}
-            <Box
+                else {setIsButtonDisabled(false)}
+                setUsername(valor);
+                
+              }}
+              
+            />
+
+
+            <Button
+              marginTop={'15px'}
+              type='submit'
+              isDisabled={isButtonDisabled}
+              isFullWidth
+              buttonColors={{
+                contrastColor: appConfig.theme.colors.neutrals["000"],
+                mainColor: appConfig.theme.colors.primary[500],
+                mainColorLight: appConfig.theme.colors.primary[400],
+                mainColorStrong: appConfig.theme.colors.primary[600],
+              }}
+            >Entrar
+            </Button>
+
+          </Box>
+          {/* Formulário */}
+
+
+          {/* Photo Area */}
+          <Box
             display={'flex'}
             flexDirection={'column'}
             alignItems={'center'}
@@ -135,27 +134,27 @@ export default function PaginaInicial() {
             borderRadius={'10px'}
             flex={1}
             minHeight={'240px'}
+          >
+            <Image
+              borderRadius={'50%'}
+              marginBottom={'16px'}
+              src={`https://github.com/${username}.png`}
+            />
+            <Text
+              variant="body4"
+              color={appConfig.theme.colors.neutrals[200]}
+              backgroundColor={appConfig.theme.colors.neutrals[800]}
+              padding={'3px 10px'}
+              borderRadius={'1000px'}
+
             >
-              <Image
-                 borderRadius={'50%'}
-                 marginBottom={'16px'}
-                src={`https://github.com/${username}.png`}
-              />
-              <Text
-                variant="body4"
-                color={appConfig.theme.colors.neutrals[200]}
-                backgroundColor={appConfig.theme.colors.neutrals[800]}
-                padding={'3px 10px'}
-                borderRadius={'1000px'}
-                
-              >
-                {username}
-              </Text>
-            </Box>
-            {/* Photo Area */}
+              {username}
+            </Text>
           </Box>
+          {/* Photo Area */}
         </Box>
-      </>
-    );
-  }
+      </Box>
+    </>
+  );
+}
 
